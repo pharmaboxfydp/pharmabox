579107147e954e5c3e2b2517d193aab47221b313
Hussein <hznagri@uwaterloo.ca>
2022-10-20 21:55:49 -0400

    setup ORM

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import connection from '../../database'
+
 type Data = {
   name: string
 }
@@ -9,10 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  connection().then((knex) => {
-    const state = knex.queryBuilder()
-    console.log(state)
-    debugger
-  })
   res.status(200).json({ name: 'John Doe' })
 }
