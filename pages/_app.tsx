b01de9f9a520bf7e3786ab3d7d7168e22c879894
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-10-22 17:42:24 -0400

    srp: initiated clerk auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
 }
 
-export default MyApp
+export default App
