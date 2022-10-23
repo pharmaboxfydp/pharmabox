b78b7977663f178a17487ef12eddb3d4112ed409
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-10-23 14:22:03 -0400

    add role, pickup enabled, and dob

--- types/types.ts
+++ types/types.ts
@@ -1,10 +1,13 @@
 export type User = {
     id: string,
-    first_name: string,
-    last_name: string,
-    email: string,
-    phone: string,
+    first_name?: string,
+    last_name?: string,
+    email?: string,
+    phone?: string,
     banned: boolean,
+    pickup_enabled: boolean,
+    role?: string,
+    dob?: string,
     createdAt: string,
     updatedAt: string
 }
