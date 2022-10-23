3853eb5252fc5b5c6f5d267080199507057fefb8
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-10-23 12:30:58 -0400

    fix id type

--- pages/api/createuser.ts
+++ pages/api/createuser.ts
@@ -23,10 +23,10 @@ export default async function handler(
                 banned,
             } = req.body.data
             let email_address = null;
-            email_address = email_addresses.find(({ id }: { id: number }) => id === primary_email_address_id) ?? null
+            email_address = email_addresses.find(({ id }: { id: string }) => id === primary_email_address_id) ?? null
 
             let phone_number = null;
-            phone_number = phone_numbers.find(({ id }: { id: number }) => id === primary_phone_number_id) ?? null
+            phone_number = phone_numbers.find(({ id }: { id: string }) => id === primary_phone_number_id) ?? null
 
             const payload: User = {
                 id: id,
