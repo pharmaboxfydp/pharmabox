e313855af05046995953ce9dad64317b8f118b1b
Hussein <hznagri@uwaterloo.ca>
2022-10-25 15:03:21 -0400

    prettier

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { withClerkMiddleware } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getAuth } from "@clerk/nextjs/server";
+import { getAuth } from '@clerk/nextjs/server'
 
 export default withClerkMiddleware((req: NextRequest) => {
-  const { userId } = getAuth(req);
-   if(!userId){
+  const { userId } = getAuth(req)
+  if (!userId) {
     const signInUrl = new URL('/sign-in', req.url)
     signInUrl.searchParams.set('redirect_url', req.url)
     return NextResponse.redirect(signInUrl)
-  } 
-  return NextResponse.next();
-});
+  }
+  return NextResponse.next()
+})
 
 // Stop Middleware running on static files like images
-export const config = { matcher: [ "/api/:path*"] }
+export const config = { matcher: ['/api/:path*'] }
