e3fe8b165a8954e9e65eb828b6980d0139c04d83
Hussein Nagri <47333088+hnagri52@users.noreply.github.com>
2022-10-25 19:24:57 -0400

    addded auth (#12)
    * addded auth
    
    * prettier
    
    * stopped middleware from running on static files

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,17 @@
 import { withClerkMiddleware } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { getAuth } from '@clerk/nextjs/server'
 
 export default withClerkMiddleware((req: NextRequest) => {
+  const { userId } = getAuth(req)
+  if (!userId) {
+    const signInUrl = new URL('/sign-in', req.url)
+    signInUrl.searchParams.set('redirect_url', req.url)
+    return NextResponse.redirect(signInUrl)
+  }
   return NextResponse.next()
 })
 
 // Stop Middleware running on static files like images
-export const config = { matcher: '/((?!.*\\.).*)' }
+export const config = { matcher: ['/api/:path*', '/((?!.*\\.).*)'] }
