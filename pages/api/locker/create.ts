78be4defee187b8d2710aba6abf8e54686f2eee2
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-10-26 00:07:27 -0400

    wip

--- pages/api/locker/create.ts
+++ pages/api/locker/create.ts
@@ -9,8 +9,28 @@ export default async function handler(
 ) {
   if (req.method === 'POST') {
     try {
-      res.status(200).json({ message: ' Success' })
+      let { locationId, locker_count } = req.body
+
+      let lockerBoxes = Array.from({ length: locker_count }, (_, i) => {
+        let obj = { id: i + 1, status: 'empty' }
+        return obj
+      })
+      const locker = await prisma.locker.create({
+        data: {
+          Location: {
+            connect: {
+              id: locationId
+            }
+          },
+          lockerBoxes: {
+            createMany: { data: lockerBoxes }
+          }
+        }
+      })
+
+      res.status(200).json({ message: ' Success', locker })
     } catch (e) {
+      console.log(e)
       res.status(400).json({ message: 'Bad Request', error: e })
     }
   } else {
