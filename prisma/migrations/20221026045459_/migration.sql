87ae7f6334168fd35d41684d56309ee096071a40
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-10-26 13:58:05 -0400

    remove dup migration

--- prisma/migrations/20221026045459_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[label,lockerId]` on the table `locker_box` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "locker_box_label_lockerId_key" ON "locker_box"("label", "lockerId");
