988f19c525fff2ff65c12f393021e5d25cf77d3b
Hussein Nagri <47333088+hnagri52@users.noreply.github.com>
2022-10-26 16:05:06 -0400

    Patients crud (#16)
    * added crud for patients
    
    * prettier
    
    * comment address
    
    * addressed changes

---
+++ pages/api/patients/id.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    try {
+      const { id } = req.body.data
+
+      const patient = await prisma.patient.findUniqueOrThrow({
+        where: {
+          id: id
+        }
+      })
+
+      res.status(200).json({ message: 'Success', patient })
+    } catch (e) {
+      res.status(400).json({ message: 'Bad Request', error: e })
+    }
+  } else {
+    res.status(405).json({ message: `Method: ${req.method} Not Allowed` })
+  }
+}
