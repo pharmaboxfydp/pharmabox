a5295e29756f5ee11e32dfb6b575a4fe4db196a9
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-10-26 22:33:52 -0400

    remove middleware

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,12 @@ import { getAuth, withClerkMiddleware } from '@clerk/nextjs/server'
 import { NextRequest, NextResponse } from 'next/server'
 
 export default withClerkMiddleware((req: NextRequest) => {
-  const { userId } = getAuth(req)
-  if (!userId) {
-    const signInUrl = new URL('/sign-in', req.url)
-    signInUrl.searchParams.set('redirect_url', req.url)
-    return NextResponse.redirect(signInUrl)
-  }
+  // const { userId } = getAuth(req)
+  // if (!userId) {
+  //   const signInUrl = new URL('/sign-in', req.url)
+  //   signInUrl.searchParams.set('redirect_url', req.url)
+  //   return NextResponse.redirect(signInUrl)
+  // }
   return NextResponse.next()
 })
 
