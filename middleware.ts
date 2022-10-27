27945519d9f563f88b1b2c99fb6d10605686455e
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-10-26 22:57:18 -0400

    exclude user and lockers

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,15 @@ import { getAuth, withClerkMiddleware } from '@clerk/nextjs/server'
 import { NextRequest, NextResponse } from 'next/server'
 
 export default withClerkMiddleware((req: NextRequest) => {
-  // const { userId } = getAuth(req)
-  // if (!userId) {
-  //   const signInUrl = new URL('/sign-in', req.url)
-  //   signInUrl.searchParams.set('redirect_url', req.url)
-  //   return NextResponse.redirect(signInUrl)
-  // }
+  const { userId } = getAuth(req)
+  if (!userId) {
+    const signInUrl = new URL('/sign-in', req.url)
+    signInUrl.searchParams.set('redirect_url', req.url)
+    return NextResponse.redirect(signInUrl)
+  }
   return NextResponse.next()
 })
 
-export const config = { matcher: ['/api/:path*'] }
+export const config = {
+  matcher: ['/api/patients/', '/api/prescriptions/']
+}
