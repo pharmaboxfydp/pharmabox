7f056b1846e0958e824db089f30e019390a57239
Aliya Rajwani <44960260+rajwanialiya@users.noreply.github.com>
2022-10-28 14:02:23 -0400

    CRUD endpoints for prescriptions (#23)
    * to be tested
    
    * tested
    
    * applied prettier
    
    * removed console statements
    
    Co-authored-by: Aliya <aliya@Aliyas-MacBook-Pro.local>

---
+++ pages/api/prescriptions/create.ts
@@ -0,0 +1,50 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    try {
+      const {
+        name,
+        status,
+        pickupTime,
+        patientId,
+        balance,
+        locationId,
+        lockerBoxId
+      } = req.body
+      let prescription = await prisma.prescription.create({
+        data: {
+          name: name,
+          status: status,
+          pickupTime: pickupTime,
+          balance: balance,
+          Patient: {
+            connect: {
+              id: patientId
+            }
+          },
+          LockerBox: {
+            connect: {
+              id: lockerBoxId
+            }
+          },
+          Location: {
+            connect: {
+              id: locationId
+            }
+          }
+        }
+      })
+      res.status(200).json({ message: 'Success', prescription })
+    } catch (e) {
+      res.status(400).json({ message: 'Bad Request', error: e })
+    }
+  } else {
+    res.status(405).json({ message: `Method: ${req.method} Not Allowed` })
+  }
+}
---
+++ pages/api/prescriptions/read.ts
@@ -0,0 +1,35 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // retrieve prescriptions by id, patientId, or don't specify body to retrieve all
+  if (req.method === 'POST') {
+    try {
+      const { id, patientId } = req.body
+      let prescription = null
+
+      if (id) {
+        prescription = await prisma.prescription.findUniqueOrThrow({
+          where: {
+            id: id
+          }
+        })
+      } else {
+        prescription = await prisma.prescription.findMany({
+          where: {
+            patientId: patientId
+          }
+        })
+      }
+      res.status(200).json({ message: 'Success', prescription })
+    } catch (e) {
+      res.status(400).json({ message: 'Bad Request', error: e })
+    }
+  } else {
+    res.status(405).json({ message: `Method: ${req.method} Not Allowed` })
+  }
+}
