faa0c51a9f5f7d0910fa3bb3225cb447375d5ca2
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-10-30 20:24:53 -0400

    test

---
+++ pages/api/test/test.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // retrieve location by id or don't specify body to retrieve all
+  if (req.method === 'GET') {
+    try {
+      res.status(200).json({ message: 'Success' })
+    } catch (e) {
+      res.status(400).json({ message: 'Bad Request', error: e })
+    }
+  } else {
+    res.status(405).json({ message: `Method: ${req.method} Not Allowed` })
+  }
+}
