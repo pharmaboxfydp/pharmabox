c232db7d01ba81f839317e7518e08d596ba6b1df
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-10-31 21:16:01 -0400

    fix comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,9 +18,6 @@ NProgress.configure({
   showSpinner: false
 })
 
-// Router.events.on('routeChangeStart', () => NProgress.start())
-// Router.events.on('routeChangeComplete', () => NProgress.done())
-// Router.events.on('routeChangeError', () => NProgress.done())
 const publicPages = ['/', '/sign-up', '/sign-in']
 
 const StyledContainer = styled(ToastContainer)`
