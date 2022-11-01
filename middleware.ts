bd50996c0870b94de204718023524b34e31e9e91
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-10-31 21:10:08 -0400

    add back middleware

--- middleware.ts
+++ middleware.ts
@@ -2,19 +2,19 @@ import { getAuth, withClerkMiddleware } from '@clerk/nextjs/server'
 import { NextRequest, NextResponse } from 'next/server'
 
 export default withClerkMiddleware((req: NextRequest) => {
-  // const { userId } = getAuth(req)
-  // if (!userId) {
-  //   const signInUrl = new URL('/sign-in', req.url)
-  //   signInUrl.searchParams.set('redirect_url', req.url)
-  //   return NextResponse.redirect(signInUrl)
-  // }
+  const { userId } = getAuth(req)
+  if (!userId) {
+    const signInUrl = new URL('/sign-in', req.url)
+    signInUrl.searchParams.set('redirect_url', req.url)
+    return NextResponse.redirect(signInUrl)
+  }
   return NextResponse.next()
 })
 
 export const config = {
   matcher: [
     '/api/patients/:path*',
-    '/api/prescriptions/:path*'
-    // '/api/locations/:path*'
+    '/api/prescriptions/:path*',
+    '/api/locations/:path*'
   ]
 }
