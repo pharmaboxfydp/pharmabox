5a02846964897916208a23ab3f4a176adefb7ceb
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-11-02 20:36:50 -0400

    quick and dirty teams page so far

---
+++ hooks/team.ts
@@ -0,0 +1,23 @@
+import { User } from '../types/types'
+import useSWR from 'swr'
+
+export interface UseTeam {
+  team: User[] | null
+  isLoading: boolean
+  isError: Error
+}
+
+const fetcher = (
+  ...arg: [string, Record<string, any>]
+): Promise<{ message: string; teamMembers: User[] }> =>
+  fetch(...arg).then((res) => res.json())
+
+export default function useTeam(user: User): UseTeam {
+  const { data, error } = useSWR(`/api/team/${user.Staff?.locationId}`, fetcher)
+
+  return {
+    team: data?.teamMembers ?? null,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
--- pages/team.tsx
+++ pages/team.tsx
@@ -4,8 +4,15 @@ import { withServerSideAuth } from '@clerk/nextjs/ssr'
 import { SSRUser } from '../helpers/user-details'
 import Page from '../components/Page'
 import { ServerPageProps } from '../types/types'
-import { Box, Text } from 'grommet'
+import { Box, Card, DataTable, ResponsiveContext, Text } from 'grommet'
+import Breadcrumbs from '../components/Breadcrumbs'
+import { Logout } from '@carbon/icons-react'
+import SidebarButton from '../components/SidebarButton'
+import { useClerk } from '@clerk/nextjs'
+import useTeam from '../hooks/team'
 const Team = ({ user }: ServerPageProps) => {
+  const { signOut } = useClerk()
+  const { team, isLoading, isError } = useTeam(user)
   return (
     <>
       <Head>
@@ -17,8 +24,48 @@ const Team = ({ user }: ServerPageProps) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Page user={user}>
-        <Box pad="medium">
-          <Text>Team</Text>
+        <Box
+          animation="fadeIn"
+          direction="column"
+          align="start"
+          fill
+          className="Settings"
+        >
+          <Breadcrumbs pages={['Team']} />
+          <Box direction="row" border="top" fill>
+            <Box pad="medium" basis="auto" fill="horizontal" gap="medium">
+              <Text>Team Members</Text>
+              <Card pad="medium">
+                {!isLoading && !isError && (
+                  <DataTable
+                    columns={[
+                      {
+                        property: 'firstName',
+                        primary: true,
+                        header: <Text>First Name</Text>
+                      },
+                      { property: 'lastName', header: <Text>Last Name</Text> },
+                      { property: 'email', header: <Text>Email</Text> }
+                    ]}
+                    data={team ?? []}
+                  />
+                )}
+              </Card>
+              <div>
+                <ResponsiveContext.Consumer>
+                  {(responsive) =>
+                    responsive === 'small' && (
+                      <SidebarButton
+                        icon={<Logout size={24} />}
+                        label={'Logout'}
+                        onClick={signOut}
+                      />
+                    )
+                  }
+                </ResponsiveContext.Consumer>
+              </div>
+            </Box>
+          </Box>
         </Box>
       </Page>
     </>
@@ -28,6 +75,7 @@ const Team = ({ user }: ServerPageProps) => {
 export default Team
 
 export const getServerSideProps = withServerSideAuth(
-  async ({ req, res }) => SSRUser({ req, res }),
+  async ({ req, res }) =>
+    SSRUser({ req, res, query: { include: { Staff: true } } }),
   { loadUser: true }
 )
