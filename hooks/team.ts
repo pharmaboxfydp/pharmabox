5cbee36e3b4f77f7cb5dc9fda494b66ecaab5a66
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-11-03 21:42:04 -0400

    updated hooks/team.ts

--- hooks/team.ts
+++ hooks/team.ts
@@ -1,10 +1,17 @@
 import { User } from '../types/types'
-import useSWR from 'swr'
+import useSWR, { useSWRConfig } from 'swr'
+
+export interface AddTeamMember {
+  email: string
+  locationId: number
+  isAdmin: boolean
+}
 
 export interface UseTeam {
   team: User[] | null
   isLoading: boolean
   isError: Error
+  addTeamMember: ({ email, locationId, isAdmin }: AddTeamMember) => void
 }
 
 const fetcher = (
@@ -13,21 +20,26 @@ const fetcher = (
   fetch(...arg).then((res) => res.json())
 
 export default function useTeam(user: User): UseTeam {
+  const { mutate } = useSWRConfig()
   const { data, error } = useSWR(`/api/team/${user.Staff?.locationId}`, fetcher)
 
-  async function addTeamMember({
-    email,
-    locationId,
-    isAdmin
-  }: {
-    email: string
-    locationId: number
-    isAdmin: boolean
-  }) {}
+  async function addTeamMember({ email, locationId, isAdmin }: AddTeamMember) {
+    const response = await fetch('/api/team/members/invite', {
+      method: 'POST',
+      body: JSON.stringify({
+        data: { email, locationId, isAdmin }
+      })
+    })
+    if (response.status === 200) {
+      const res = await response.json()
+      mutate(`/api/team/${user.Staff?.locationId}`)
+    }
+  }
 
   return {
     team: data?.teamMembers ?? null,
     isLoading: !error && !data,
-    isError: error
+    isError: error,
+    addTeamMember
   }
 }
