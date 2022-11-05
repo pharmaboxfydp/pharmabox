0fad4afe659573b49bb4cd157f9999393c6eda36
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-11-04 21:58:38 -0400

    Make New Fields Have Default

---
+++ prisma/migrations/20221105015816_/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "locations" ALTER COLUMN "phoneNumber" SET DEFAULT '',
+ALTER COLUMN "city" SET DEFAULT '',
+ALTER COLUMN "country" SET DEFAULT '',
+ALTER COLUMN "province" SET DEFAULT '',
+ALTER COLUMN "streetAddress" SET DEFAULT '';
