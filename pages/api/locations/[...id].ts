db7234bda8ccdc3a08969c573af6b00cc921bbd0
Joshua Wilkinson <joshua.wilkinson@meraki.net>
2022-11-06 01:19:59 -0400

    update locations by id

--- pages/api/locations/[...id].ts
+++ pages/api/locations/[...id].ts
@@ -9,28 +9,20 @@ export default async function handler(
   if (req.method === 'GET') {
     try {
       const { id } = req.query
-      let locationId = null
-      if (id?.length === 1) {
-        locationId = parseInt(id[0])
-      } else if (id?.length && id?.length > 1) {
+      if (id?.length !== 1) {
         throw new Error('Invalid Request. Expected 1 Id')
       }
+      const locationId = parseInt(id[0])
+      const location = await prisma.location.findUniqueOrThrow({
+        where: {
+          id: locationId
+        },
+        include: {
+          Prescriptions: true,
+          LockerBoxes: true
+        }
+      })
 
-      let location = null
-
-      if (locationId) {
-        location = await prisma.location.findUniqueOrThrow({
-          where: {
-            id: locationId
-          },
-          include: {
-            Prescriptions: true,
-            LockerBoxes: true
-          }
-        })
-      } else {
-        location = await prisma.location.findMany()
-      }
       res.status(200).json({ message: 'Success', location })
     } catch (e) {
       res.status(400).json({ message: 'Bad Request', error: e?.toString() })
