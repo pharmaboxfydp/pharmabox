447846f6d110ada31c8b44ea3d6f82abf4e09349
Sammy Robens-Paradise <srobensparadise@gmail.com>
2023-02-13 16:46:31 -0500

    continued development

--- components/AuthorizedUsers.tsx
+++ components/AuthorizedUsers.tsx
@@ -2,6 +2,7 @@ import {
   Avatar,
   Box,
   Card,
+  CardBody,
   CardHeader,
   Grid,
   ResponsiveContext,
@@ -12,28 +13,41 @@ import { capitalize } from 'lodash'
 import { useContext } from 'react'
 import useTeam from '../hooks/team'
 import useClerkUser from '../hooks/users'
-import { User } from '../types/types'
+import theme from '../styles/theme'
+import { Role, User } from '../types/types'
 
 function AuthorizedUserCard({ user }: { user: User }) {
   const { user: clerkUser } = useClerkUser(user.id)
   const profileImageUrl = clerkUser?.profile_image_url
   return (
     <Card
-      height="xsmall"
       background="light-1"
-      width="medium"
       animation="slideUp"
       pad="small"
+      width="small"
+      gap="small"
     >
       <CardHeader>
-        <Box direction="row" gap="small">
+        <Box direction="row" flex="grow" gap="small">
           <Avatar src={profileImageUrl} size="xsmall" animation="fadeIn" />
           <Text size="xsmall">
             {user.firstName} {user.lastName}
           </Text>
         </Box>
-        <Tag name="Role" value={capitalize(user.role)} size="xsmall" />
       </CardHeader>
+      <CardBody>
+        <Box
+          round
+          background={
+            user.role === Role.Pharmacist
+              ? theme.global.colors['neutral-3']
+              : theme.global.colors['neutral-4']
+          }
+          style={{ color: theme.global.colors.white, width: 'fit-content' }}
+        >
+          <Tag name="Role" value={capitalize(user.role)} size="small" />
+        </Box>
+      </CardBody>
     </Card>
   )
 }
