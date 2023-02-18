229ed82a3c843e2fa15769858dc72b5a74d55217
Sammy Robens-Paradise <srobensparadise@gmail.com>
2023-02-17 20:28:00 -0500

    srp: fixed bug that causes multiple instances of prisma clients to be created that caused a failure and slowdown on local development

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,15 @@
-// lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 let prisma: PrismaClient
-prisma = new PrismaClient()
+prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['info', 'query', 'warn', 'error']
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
 
 export default prisma
