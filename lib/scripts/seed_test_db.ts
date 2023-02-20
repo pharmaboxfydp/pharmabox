0effac3aca7c8c17ff2be507245f3c4ec574421a
Sammy Robens-Paradise <srobensparadise@gmail.com>
2023-02-20 14:38:41 -0500

    Standardized `seed_test_dbs` test rig so that we always have consistent data (#63)
    * changed seed test dbs script to allow for consistent data
    
    * updated locker count for more testing
    
    * added newline
    
    * updated script to create patients!
    
    * fixed prettier problems

--- lib/scripts/seed_test_db.ts
+++ lib/scripts/seed_test_db.ts
@@ -8,16 +8,17 @@ import * as dotenv from 'dotenv'
 import { LockerBoxState, Role, User } from '../../types/types'
 import { UserJSON } from '@clerk/backend-core'
 import data from './test_data'
+import { LockerBox } from '@prisma/client'
 import { faker } from '@faker-js/faker'
 
 dotenv.config()
 
 const BASE_URL = `https://api.clerk.dev/v1`
 
-function randInt(min: number, max: number) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
+/**
+ * Seed faker.js for repeatable outputs
+ */
+faker.seed(42)
 
 /**
  * Get users from our dev database
@@ -35,17 +36,10 @@ async function getDevUsers(): Promise<UserJSON[]> {
   return users
 }
 
-/**
- * Seeds users in local database
- * @param users
- * @returns Promise<Prisma.BatchPayload>
- */
-async function seedUsers(
-  users: UserJSON[]
-): Promise<{ count: number; patientUsers: User[]; staffUsers: User[] }> {
+async function seedPharmacistsAndStaffUsersFromClerk(users: UserJSON[]) {
   const staffUsers: User[] = []
-  const patientUsers: User[] = []
-
+  const pharmacistUsers: User[] = []
+  users.sort()
   users.forEach(async (user, index) => {
     const numUses = users.length
     const userNumber = index + 1
@@ -66,93 +60,205 @@ async function seedUsers(
       createdAt: createdAt.toISOString(),
       updatedAt: updatedAt.toISOString(),
       lastLoggedIn: updatedAt.toISOString(),
-      // make half the users patients, and the other half staff locally
-      role: userNumber < midpoint ? Role.Patient : Role.Staff
+      // make half the users pharmacists, and the other half staff locally
+      role: userNumber < midpoint ? Role.Pharmacist : Role.Staff
     }
 
-    if (newUser.role === Role.Patient) {
-      patientUsers.push(newUser)
+    if (newUser.role === Role.Pharmacist) {
+      pharmacistUsers.push(newUser)
     }
     if (newUser.role === Role.Staff) {
       staffUsers.push(newUser)
     }
   })
 
-  const count = staffUsers.length + patientUsers.length
+  const count = staffUsers.length + pharmacistUsers.length
   try {
-    staffUsers.forEach(async (staffUser: User, index) => {
-      const numUses = staffUsers.length
-      const userNumber = index + 1
-      const midpoint = Math.max(numUses / 2)
-
-      await prisma.user.create({
-        // @ts-ignore
-        data: {
-          ...staffUser,
-          Staff: {
-            connectOrCreate: {
-              where: {
-                userId: staffUser.id
-              },
-              create: {
-                isAdmin: userNumber < midpoint ?? false
+    Promise.all(
+      staffUsers.map(async (staffUser: User, index) => {
+        const numUses = staffUsers.length
+        const userNumber = index + 1
+        const midpoint = Math.max(numUses / 2)
+        await prisma.user.create({
+          /**
+           * Ignore error here since we are going to get an expected mismatch
+           */
+          // @ts-ignore
+          data: {
+            ...staffUser,
+            Staff: {
+              connectOrCreate: {
+                where: {
+                  userId: staffUser.id
+                },
+                create: {
+                  isAdmin: userNumber < midpoint ?? false
+                }
               }
+            }
+          }
+        })
+      })
+    )
+  } catch (error: unknown) {
+    throw new Error(error as string)
+  }
+  try {
+    Promise.all(
+      pharmacistUsers.map(async (PharmacistUser, index) => {
+        const numUses = pharmacistUsers.length
+        const userNumber = index + 1
+        const midpoint = Math.max(numUses / 2)
+        await prisma.user.create({
+          /**
+           * Ignore error here since we are going to get an expected mismatch
+           */
+          // @ts-ignore
+          data: {
+            ...PharmacistUser,
+            Pharmacist: {
+              connectOrCreate: {
+                where: {
+                  userId: PharmacistUser.id
+                },
+                create: {
+                  isAdmin: userNumber < midpoint ?? false
+                }
+              }
+            }
+          }
+        })
+      })
+    )
+  } catch (error: unknown) {
+    throw new Error(error as string)
+  }
+
+  return { count, pharmacistUsers, staffUsers }
+}
+
+async function seedPatients() {
+  const numPatientsToCreate = 200
+  const patients: User[] = []
+  console.info(
+    '\x1b[36m%s\x1b[0m',
+    `CREATING ${numPatientsToCreate} RANDOM PATIENTS....`
+  )
+
+  const numCharacters = 24
+
+  /**
+   * Create random users (not connected to clerk so that cannot sign in
+   */
+  var idx = 0
+  while (idx < numPatientsToCreate) {
+    const id = `patient_user_${faker.datatype.uuid()}`
+    const newUser: User = {
+      id,
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      createdAt: faker.date.past().toISOString(),
+      updatedAt: faker.date.past().toISOString(),
+      phoneNumber: faker.phone.number(),
+      role: Role.Patient,
+      lastLoggedIn: faker.date.past().toISOString()
+    }
+    patients.push(newUser)
+    idx++
+  }
+  try {
+    Promise.all(
+      patients.map(async (patientUser: User) => {
+        await prisma.user.create({
+          /**
+           * Ignore error here since we are going to get an expected mismatch
+           */
+          // @ts-ignore
+          data: {
+            ...patientUser,
+            Patient: {
+              connectOrCreate: {
+                where: {
+                  userId: patientUser.id
+                },
+                create: {
+                  pickupEnabled: faker.helpers.arrayElement([true, false]),
+                  dob: faker.date.birthdate().toISOString()
+                }
+              }
+            }
+          }
+        })
+      })
+    )
+  } catch (error: unknown) {
+    throw new Error(error as string)
+  }
+  return { count: numPatientsToCreate, patientUsers: patients }
+}
+
+/**
+ * Seeds users in local database
+ * @param users
+ * @returns Promise<Prisma.BatchPayload>
+ */
+async function seedUsers(users: UserJSON[]): Promise<{
+  count: number
+  pharmacistUsers: User[]
+  staffUsers: User[]
+  patientUsers: User[]
+}> {
+  const {
+    count: staffAndPharmacistCount,
+    pharmacistUsers,
+    staffUsers
+  } = await seedPharmacistsAndStaffUsersFromClerk(users)
+  const { count: patientCount, patientUsers } = await seedPatients()
+  const count = patientCount + staffAndPharmacistCount
+  return { count, pharmacistUsers, staffUsers, patientUsers }
+}
+
+async function seedLocations(staff: User[], pharmacist: User[]) {
+  const { locations } = data
+
+  const [, , seededLocations] = await prisma.$transaction([
+    prisma.location.deleteMany({}),
+    prisma.location.createMany({ data: locations }),
+    prisma.location.findMany({})
+  ])
+
+  await Promise.all(
+    staff.map(async (staffMember, index) => {
+      await prisma.staff.update({
+        where: { userId: staffMember.id },
+        data: {
+          Location: {
+            connect: {
+              id: seededLocations[index > Math.floor(staff.length) / 2 ? 1 : 0]
+                ?.id
             }
           }
         }
       })
     })
-  } catch (error) {
-    console.error(error)
-  }
-  patientUsers.forEach(async (patientUser, index) => {
-    const numUses = patientUsers.length
-    const userNumber = index + 1
-    const midpoint = Math.max(numUses / 2)
-    await prisma.user.create({
-      // @ts-ignore
-      data: {
-        ...patientUser,
-        Patient: {
-          connectOrCreate: {
-            where: {
-              userId: patientUser.id
-            },
-            create: {
-              pickupEnabled: true,
-              dob:
-                userNumber < midpoint
-                  ? faker.date.birthdate().toDateString()
-                  : null
+  )
+
+  await Promise.all(
+    pharmacist.map(async (pharmacistMember, index) => {
+      await prisma.pharmacist.update({
+        where: { userId: pharmacistMember.id },
+        data: {
+          Location: {
+            connect: {
+              id: seededLocations[index > Math.floor(staff.length) / 2 ? 1 : 0]
+                ?.id
             }
           }
         }
-      }
+      })
     })
-  })
-
-  return { count, patientUsers, staffUsers }
-}
-
-async function seedLocations(staff: User[]) {
-  const { locations } = data
-  await prisma.location.deleteMany({})
-  await prisma.location.createMany({ data: locations })
-  const seededLocations = await prisma.location.findMany({})
-
-  staff.forEach(async (staffMember, index) => {
-    await prisma.staff.update({
-      where: { userId: staffMember.id },
-      data: {
-        Location: {
-          connect: {
-            id: seededLocations[index > Math.floor(staff.length) / 2 ? 1 : 0]
-              ?.id
-          }
-        }
-      }
-    })
-  })
+  )
 
   return { seededLocations }
 }
@@ -162,25 +268,30 @@ async function seedLockerBoxes() {
   prisma.location.findMany({}).then((locations) => {
     const locationIds = locations.map((location) => location.id)
     locationIds.forEach(async (locationId: number) => {
-      const lockerCount = randInt(1, 5)
-      const lockerBoxes = Array.from({ length: lockerCount }, (_, index) => {
-        return {
-          label: index + 1,
-          status: LockerBoxState.empty,
-          locationId
-        }
-      })
-      lockerBoxes.forEach(async (box) => {
-        await prisma.lockerBox.create({
-          data: {
-            label: box.label,
-            status: box.status,
-            Location: {
-              connect: { id: box.locationId }
-            }
+      const lockerCount = 8
+      const lockerBoxes = Array.from(
+        { length: lockerCount },
+        (_, index: number): Partial<LockerBox> => {
+          return {
+            label: index + 1,
+            status: LockerBoxState.empty,
+            locationId
           }
+        }
+      )
+      await Promise.all(
+        lockerBoxes.map(async (box: Partial<LockerBox>) => {
+          await prisma.lockerBox.create({
+            data: {
+              label: box.label as number,
+              status: box.status as string,
+              Location: {
+                connect: { id: box.locationId }
+              }
+            }
+          })
         })
-      })
+      )
     })
   })
 }
@@ -193,28 +304,33 @@ getDevUsers()
   .then((users) => {
     seedUsers(users)
       .then(async (payload) => {
-        console.info(`Created ${payload.count} Local Users`)
-        console.info('\x1b[36m%s\x1b[0m', 'PATIENTS -----')
-        console.table(payload.patientUsers, [
-          'id',
-          'firstName',
-          'email',
-          'role'
-        ])
-        console.info('\x1b[36m%s\x1b[0m', 'STAFF -----')
-        console.table(payload.staffUsers, ['id', 'firstName', 'email', 'role'])
-        seedLocations(payload.staffUsers).then((res) => {
-          console.info('\x1b[36m%s\x1b[0m', 'LOCATIONS -----')
-          console.table(res.seededLocations, ['id'])
+        const { count, staffUsers, pharmacistUsers, patientUsers } = payload
+        /**
+         * Log Information about the users created
+         */
+        console.info(`Created ${count} Local Users`)
+        console.info('\x1b[36m%s\x1b[0m', 'STAFF ✅ -----')
+        console.table(staffUsers, ['id', 'firstName', 'email', 'role'])
+        console.info('\x1b[36m%s\x1b[0m', 'PHARMACISTS ✅ -----')
+        console.table(pharmacistUsers, ['id', 'firstName', 'email', 'role'])
+        console.info('\x1b[36m%s\x1b[0m', 'PATIENTS (NOT IN CLERK) ✅ -----')
+        console.table(patientUsers, ['id', 'firstName', 'email', 'role'])
+        /**
+         * Create Locations and create teams of users
+         */
+        seedLocations(staffUsers, pharmacistUsers).then((payload) => {
+          const { seededLocations } = payload
+          console.info('\x1b[36m%s\x1b[0m', 'LOCATIONS ✅ -----')
+          console.table(seededLocations, ['id'])
           seedLockerBoxes().then(() => {
-            console.info('\x1b[36m%s\x1b[0m', 'CREATED LOCKER BOXES')
+            console.info('\x1b[36m%s\x1b[0m', 'CREATED LOCKER BOXES ✅')
           })
         })
       })
-      .catch((error) => {
+      .catch((error: string) => {
         throw new Error(error)
       })
   })
-  .catch((error) => {
+  .catch((error: string) => {
     throw new Error(error)
   })
