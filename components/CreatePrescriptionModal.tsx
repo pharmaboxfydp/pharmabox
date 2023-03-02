6ba78b4ff198a389248e8081d4f2d29c825613ac
Sammy Robens-Paradise <srobensparadise@gmail.com>
2023-03-02 15:21:06 -0500

    continued development

--- components/CreatePrescriptionModal.tsx
+++ components/CreatePrescriptionModal.tsx
@@ -62,6 +62,45 @@ export default function CreatePrescriptionModal({ user }: ServerPageProps) {
   }
 
   if (showCreatePrescription) {
+    if (emptyLockerboxes?.length === 0) {
+      return (
+        <Layer
+          onEsc={closeModal}
+          onClickOutside={closeModal}
+          position="center"
+          style={{ display: 'block', overflow: 'scroll' }}
+          full
+        >
+          <Box
+            direction="column"
+            overflow="scroll"
+            gap="small"
+            data-cy="create-prescription-modal"
+            pad="large"
+          >
+            <Box flex={false} direction="row" justify="between">
+              <Heading level={4} margin="none">
+                Create Prescription
+              </Heading>
+              <Button
+                icon={<Close size={16} />}
+                onClick={closeModal}
+                data-cy="close-modal"
+              />
+            </Box>
+            <Box
+              pad="medium"
+              border={{ color: theme.global.colors['status-critical'] }}
+              round="small"
+            >
+              <Text color={theme.global.colors['status-critical']}>
+                All Locker Boxes Full
+              </Text>
+            </Box>
+          </Box>
+        </Layer>
+      )
+    }
     return (
       <Layer
         onEsc={closeModal}
@@ -89,156 +128,164 @@ export default function CreatePrescriptionModal({ user }: ServerPageProps) {
                 />
               </Box>
               <Box margin={{ vertical: 'medium' }} overflow="scroll">
-                <Form onSubmit={handleSubmit}>
-                  <Box gap="medium">
-                    <Box border pad="small" round="small">
-                      <PatientsTable
-                        patientsPageState={patientsSearchPaginationState}
-                        defaultPage={1}
-                        defaultStep={3}
-                        onRowClickOverride={handleRowClick}
-                        filterOnEnabled
-                      />
-                    </Box>
-                    {selectedPatient ? (
-                      <Box
-                        border={{ color: theme.global.colors['neutral-2'] }}
-                        round="small"
-                      >
+                {emptyLockerboxes.length && (
+                  <Form onSubmit={handleSubmit}>
+                    <Box gap="medium">
+                      <Box border pad="small" round="small">
+                        <PatientsTable
+                          patientsPageState={patientsSearchPaginationState}
+                          defaultPage={1}
+                          defaultStep={3}
+                          onRowClickOverride={handleRowClick}
+                          filterOnEnabled
+                        />
+                      </Box>
+                      {selectedPatient ? (
                         <Box
-                          direction="row"
-                          gap="small"
-                          pad="medium"
-                          align="center"
-                          justify="stretch"
+                          border={{ color: theme.global.colors['neutral-2'] }}
+                          round="small"
                         >
-                          <Text size="medium">
-                            First Name:{' '}
-                            <Anchor
-                              href={`/patients/${selectedPatient.Patient?.id}`}
-                            >
-                              {selectedPatient.firstName}
-                            </Anchor>
-                          </Text>
-                          <Text>
-                            Last Name:{' '}
-                            <Anchor
-                              href={`/patients/${selectedPatient.Patient?.id}`}
-                            >
-                              {selectedPatient.lastName}
-                            </Anchor>
-                          </Text>
-                          <Text>
-                            Phone Number:{' '}
-                            <Anchor
-                              href={`/patients/${selectedPatient.Patient?.id}`}
-                            >
-                              {formatPhoneNumber(
-                                `${selectedPatient.phoneNumber}`
-                              )}
-                            </Anchor>
-                          </Text>
-                          <Text>
-                            Email:{' '}
-                            <Anchor
-                              href={`/patients/${selectedPatient.Patient?.id}`}
-                            >
-                              {selectedPatient.email}
-                            </Anchor>
+                          <Box
+                            direction="row"
+                            gap="small"
+                            pad="medium"
+                            align="center"
+                            justify="stretch"
+                          >
+                            <Text size="medium">
+                              First Name:{' '}
+                              <Anchor
+                                href={`/patients/${selectedPatient.Patient?.id}`}
+                              >
+                                {selectedPatient.firstName}
+                              </Anchor>
+                            </Text>
+                            <Text>
+                              Last Name:{' '}
+                              <Anchor
+                                href={`/patients/${selectedPatient.Patient?.id}`}
+                              >
+                                {selectedPatient.lastName}
+                              </Anchor>
+                            </Text>
+                            <Text>
+                              Phone Number:{' '}
+                              <Anchor
+                                href={`/patients/${selectedPatient.Patient?.id}`}
+                              >
+                                {formatPhoneNumber(
+                                  `${selectedPatient.phoneNumber}`
+                                )}
+                              </Anchor>
+                            </Text>
+                            <Text>
+                              Email:{' '}
+                              <Anchor
+                                href={`/patients/${selectedPatient.Patient?.id}`}
+                              >
+                                {selectedPatient.email}
+                              </Anchor>
+                            </Text>
+                          </Box>
+                        </Box>
+                      ) : (
+                        <Box
+                          border={{
+                            color: theme.global.colors['status-critical']
+                          }}
+                          round="small"
+                          pad="medium"
+                        >
+                          <Text color={theme.global.colors['status-critical']}>
+                            Select Patient From Table
                           </Text>
                         </Box>
+                      )}
+                      <Box border round="small" pad="medium">
+                        <FormField
+                          label="Prescription Name / Description"
+                          htmlFor="prescriptionName"
+                          name="prescriptionName"
+                          required
+                        >
+                          <TextArea
+                            size="small"
+                            id="prescriptionName"
+                            name="prescriptionName"
+                            placeholder="Name / Description"
+                            a11yTitle="Prescription Name Input"
+                            value={prescriptionName}
+                            onChange={(event) =>
+                              updatePrescriptionName(event.target.value)
+                            }
+                          />
+                        </FormField>
                       </Box>
-                    ) : (
-                      <Box
-                        border={{
-                          color: theme.global.colors['status-critical']
-                        }}
-                        round="small"
-                        pad="medium"
-                      >
-                        <Text color={theme.global.colors['status-critical']}>
-                          Select Patient From Table
+                      <Box border round="small" pad="medium">
+                        <FormField
+                          label="Locker Box"
+                          htmlFor="lockerBox"
+                          name="lockerBox"
+                        >
+                          <Select
+                            id="lockerBox"
+                            name="lockerBox"
+                            options={
+                              emptyLockerboxes?.map((locker) => locker.label) ??
+                              []
+                            }
+                            onChange={({ option }) => {
+                              updatePrescriptionLocker(option)
+                            }}
+                            defaultValue={
+                              emptyLockerboxes?.map(
+                                (locker) => locker.label
+                              )[0] ?? 1
+                            }
+                          />
+                        </FormField>
+                      </Box>
+                    </Box>
+                    <Box
+                      flex="grow"
+                      overflow="auto"
+                      pad={{ vertical: 'medium' }}
+                    ></Box>
+                    <Box flex={false} as="footer" gap="small">
+                      <Box pad={{ left: 'small' }}>
+                        <Text size="medium">
+                          Verify the information you have entered is corrrect.
                         </Text>
                       </Box>
-                    )}
-                    <Box border round="small" pad="medium">
-                      <FormField
-                        label="Prescription Name / Description"
-                        htmlFor="prescriptionName"
-                        name="prescriptionName"
-                        required
-                      >
-                        <TextArea
-                          size="small"
-                          id="prescriptionName"
-                          name="prescriptionName"
-                          placeholder="Name / Description"
-                          a11yTitle="Prescription Name Input"
-                          value={prescriptionName}
-                          onChange={(event) =>
-                            updatePrescriptionName(event.target.value)
-                          }
-                        />
-                      </FormField>
+                      <Button
+                        type="submit"
+                        label="Create Prescription"
+                        id="submit-create-prescription-form"
+                        primary
+                        data-cy="submit-create-prescription-form"
+                      />
                     </Box>
-                    <Box border round="small" pad="medium">
-                      <FormField
-                        label="Locker Box"
-                        htmlFor="lockerBox"
-                        name="lockerBox"
-                      >
-                        <Select
-                          id="lockerBox"
-                          name="lockerBox"
-                          options={
-                            emptyLockerboxes?.map((locker) => locker.label) ??
-                            []
-                          }
-                          onChange={({ option }) => {
-                            updatePrescriptionLocker(option)
-                          }}
-                          defaultValue={
-                            emptyLockerboxes?.map(
-                              (locker) => locker.label
-                            )[0] ?? 1
-                          }
-                        />
-                      </FormField>
-                    </Box>
-                  </Box>
-                  <Box
-                    flex="grow"
-                    overflow="auto"
-                    pad={{ vertical: 'medium' }}
-                  ></Box>
-                  <Box flex={false} as="footer" gap="small">
-                    <Box pad={{ left: 'small' }}>
-                      <Text size="medium">
-                        Verify the information you have entered is corrrect.
-                      </Text>
-                    </Box>
-                    <Button
-                      type="submit"
-                      label="Create Prescription"
-                      id="submit-create-prescription-form"
-                      primary
-                      data-cy="submit-create-prescription-form"
-                    />
-                  </Box>
-                </Form>
+                  </Form>
+                )}
               </Box>
             </Box>
           )}
-          {isFetching && (
-            <Box pad="xlarge" align="center" gap="medium" animation="fadeIn">
-              <Text>Creating Prescription...</Text>
-              <Spinner
-                size="xlarge"
-                border={false}
-                background="linear-gradient(to right, #fc466b, #683ffb)"
-              />
-            </Box>
-          )}
+
+          {
+            /**
+             * Show a Loading Modal when we are creating a prescription
+             */
+            isFetching && (
+              <Box pad="xlarge" align="center" gap="medium" animation="fadeIn">
+                <Text>Creating Prescription...</Text>
+                <Spinner
+                  size="xlarge"
+                  border={false}
+                  background="linear-gradient(to right, #fc466b, #683ffb)"
+                />
+              </Box>
+            )
+          }
         </Box>
       </Layer>
     )
